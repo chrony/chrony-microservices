0d90f10f90871a5ea542ba00f4be7e902497c207
Chrony Joseph Oliveira <chrony@hotmail.com>
2023-12-08 18:46:32 -0300

    Commit inicial

---
+++ 00-foo-bar-spring-initializr/src/test/java/br/com/chrony/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.chrony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
